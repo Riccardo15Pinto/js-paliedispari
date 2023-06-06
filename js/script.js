1a170b68a09174969ccf18bf6971846769846561
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-06-06 16:37:25 +0200

    even or odd take element from dom

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 //check console log
 console.log('ok js');
 
+/*
+PAROLA PALINDROMA
+*/
+
 //recupero elementi dal DOM
 const textElement = document.getElementById('user-result');
 const resultElement = document.getElementById('user-answer');
@@ -40,3 +44,14 @@ buttonElement.addEventListener('click', function(){
     textElement.innerText = result;
 
 });
+
+/*
+PARI O DISPARI
+*/
+
+//Recupero elementi dal DOM
+
+const choiceElement = document.getElementById('user-choice');
+const numberElement = document.getElementById('number-user');
+const startElement = document.getElementById('start-button');
+const testElement = document.getElementById('result-test');
