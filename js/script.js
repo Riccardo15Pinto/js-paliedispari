c4b133a21f461558d9cc343e791d111a73828cc9
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-06-06 16:27:24 +0200

    take element from dom

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 //check console log
 console.log('ok js');
+
+//recupero elementi dal DOM
+const textElement = document.getElementById('user-result');
+const resultElement = document.getElementById('user-answer');
+const buttonElement = document.getElementById('button');
